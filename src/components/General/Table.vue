1d7771ed4e5ae989d05afca3246278c85a90f03e
dragutin-avdalovic <dragutin.avdalovic@walter.ba>
2018-04-03 09:55:38 +0200

    Table done, no need for component insertion after and before, v-if will be used

--- src/components/General/Table.vue
+++ src/components/General/Table.vue
@@ -5,11 +5,17 @@
     :default-sort = "{prop: 'projectName', order: 'ascending'}"
     style="width: 100%"
     @selection-change="handleSelectionChange">
-    <slot name="tableContentBefore"></slot>
+    <el-table-column v-if="selectable" type="selection" width="55"></el-table-column>
     <el-table-column sortable v-for="(column, key) in columns"
      v-bind:label="column.label" v-bind:prop="column.prop" v-bind:key='key' width="140">
     </el-table-column>
-    <slot name="tableContentAfter"></slot>
+      <el-table-column width="100">
+      <template slot-scope="scope">
+        <el-button
+          type="info" icon="el-icon-edit"
+          @click="handleEdit(scope.$index, scope.row)">Edit</el-button>
+      </template>
+    </el-table-column>
   </el-table>
 
 </template>
@@ -20,9 +26,10 @@ export default {
   },
   data () {
     return {
+      multipleSelection: []
     }
   },
-  props: ['fields', 'columns'],
+  props: ['fields', 'columns', 'selectable'],
   methods: {
     handleEdit (index, row) {
       console.log(index, row)
@@ -30,6 +37,15 @@ export default {
     handleSelectionChange (val) {
       this.multipleSelection = val
       console.log(this.multipleSelection)
+    },
+    toggleSelection (rows) {
+      if (rows) {
+        rows.forEach(row => {
+          this.$refs.multipleTable.toggleRowSelection(row)
+        })
+      } else {
+        this.$refs.multipleTable.clearSelection()
+      }
     }
   }
 }
