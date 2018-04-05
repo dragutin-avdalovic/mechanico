ead036d90dced51b432493578444d332d1e1ae17
dragutin-avdalovic <dragutin.avdalovic@walter.ba>
2018-04-05 09:43:04 +0200

    Inbox messages preview in develop, the text need to be styled, the message component done...

--- src/components/partials/Message.vue
+++ src/components/partials/Message.vue
@@ -1,16 +1,24 @@
 <template>
   <div>
-    <el-row type="flex" class="row-header" justify="center">
-      <el-col :xs="8" :sm="8" :md="8" :lg="8" :xl="8">
-        <p>kakfdsfsda</p>
+    <el-row type="flex" class="row-bg-message" justify="center">
+      <el-col :xs="3" :sm="3" :md="3" :lg="3" :xl="3" size="medium" class="center">
+        <template>
+          <el-checkbox v-model="checked"></el-checkbox>
+        </template>
       </el-col>
-      <el-col :xs="8" :sm="8" :md="8" :lg="8" :xl="8">
-        <p>kaka</p>
+      <el-col :xs="2" :sm="2" :md="2" :lg="2" :xl="2" class="center">
+        <div class="green-circle"></div>
       </el-col>
-      <el-col :xs="8" :sm="8" :md="8" :lg="8" :xl="8">
-        <p>lal</p>
+      <el-col :xs="13" :sm="13" :md="13" :lg="13" :xl="13" class="center-left">
+        <span>John Doe</span>
+        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
       </el-col>
-    </el-row>
+        <el-col :xs="6" :sm="6" :md="6" :lg="6" :xl="6" class="center-left">
+          <span>
+            Sep. 2018
+          </span>
+        </el-col>
+      </el-row>
     <el-row>
     </el-row>
   </div>
@@ -51,11 +59,37 @@ export default {
         status: 'Denied',
         type: 'danger'
       }],
-      input_search: ''
+      input_search: '',
+      checked: false
     }
   }
 }
 </script>
 <style>
   @import '../../assets/font/fontello/css/fontello.css';
+.green-circle
+{
+  border-radius: 50%;
+  background-color: green;
+  width: 0.7em;
+  height: 0.7em;
+}
+.center
+{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.center-left
+{
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+}
+.row-bg-message
+{
+  padding-top: 1em;
+  padding-bottom: 1em;
+}
 </style>
