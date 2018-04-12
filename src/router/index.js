abc9a7d5e7967f527f4a8bca7de1c14981d11f81
Din111 <dintanovic12345@gmail.com>
2018-04-12 10:58:11 +0200

    fixed merge conflict

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-<<<<<<< HEAD
-import HelloWorld from '@/components/HelloWorld'
-import ProjectForm from '@/components/ProjectForm'
-=======
 import Main from '@/components/Main'
->>>>>>> fcecce6e9ae4015c927dca798f3401e55dbec071
+import ProjectForm from '@/components/partials/ProjectForm'
 
 Vue.use(Router)
 
@@ -13,18 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-<<<<<<< HEAD
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Main',
+      component: Main
     },
     {
       path: '/project-form',
       name: 'ProjectForm',
       component: ProjectForm
-=======
-      name: 'Main',
-      component: Main
->>>>>>> fcecce6e9ae4015c927dca798f3401e55dbec071
     }
   ]
 })
