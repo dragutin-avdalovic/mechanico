a3340b661454727ede8138b4269a531485df20c3
Din111 <dintanovic12345@gmail.com>
2018-04-17 09:28:24 +0200

    added sidebar and file upload

---
+++ src/components/partials/CalculatorMain.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="image">
+		<img src="@/assets/images/calculator.png" alt="snipped picture">
+ 	</div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+
+.image{
+	display: flex;
+	justify-content: center;
+	margin-top: 280px;
+}
+
+</style>
