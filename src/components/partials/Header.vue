f7651600c6feaa7e5d1e34198dcee881b00fd669
dragutin-avdalovic <dragutin.avdalovic@walter.ba>
2018-05-07 15:47:41 +0200

    Tabs on admin page sucesfully applied... --tabs design improved

--- src/components/partials/Header.vue
+++ src/components/partials/Header.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
     <el-tabs type="border-card">
-      <el-tab-pane class="tab" label="PROJECTS MANAGEMENT">
+      <el-tab-pane>
+        <div slot="label" class="header">
+          <div class="my-projects-icon"><i class="icon-project"></i></div>
+          <div class="projects-label">PROJECTS MANAGEMENT</div>
+        </div>
         <TableHeader></TableHeader>
         <Table :fields="fields" :columns="columns" :selectable="true" :editable="true"></Table>
       </el-tab-pane>
-      <el-tab-pane class="tab" label="USER MANAGEMENT">
+      <el-tab-pane class="tab">
+        <div slot="label" class="header">
+          <div class="my-projects-icon"><i class="el-icon-picture"></i></div>
+          <div class="projects-label">USERS MANAGEMENT</div>
+        </div>
         lala
       </el-tab-pane>
     </el-tabs>
@@ -31,5 +39,16 @@ export default {
   @import '../../assets/font/fontello/css/fontello.css';
   @import '../../assets/styles/varibles';
   @import '../../assets/styles/leafs';
-
+  .header {
+    display: flex;
+    flex-direction: row;
+    .my-projects-icon {
+      font-size: 1.5em;
+      padding-right: 1em;
+    }
+    .projects-label {
+      font-weight: 500;
+      font-size: 1em;
+    }
+  }
 </style>
