1f6f9d58c3f422994031e98ee81e26822aea93d5
Din111 <dintanovic12345@gmail.com>
2018-05-07 10:13:18 +0200

    Merge with develop done

--- src/components/partials/CalculatorSidebar.vue
+++ src/components/partials/CalculatorSidebar.vue
@@ -19,7 +19,7 @@
     <div class="content">
       <el-row :gutter="5">
         <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
-          
+
 
           <CalculatorUpload></CalculatorUpload>
         </el-col>
