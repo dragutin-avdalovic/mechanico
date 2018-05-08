1d97e69adc04c28998b2544dc44c1de9a7f77e8b
dragutin-avdalovic <dragutin.avdalovic@walter.ba>
2018-05-08 11:57:30 +0200

    Request tabs development in progress

---
+++ src/components/partials/AdminRequestPage.vue
@@ -0,0 +1,139 @@
+<template>
+  <div>
+    <HeaderRequestAdmin :fields="fields" :columns="columns" :selectable="true" :editable="true"></HeaderRequestAdmin>
+  </div>
+</template>
+<script>
+import Table from './Table.vue'
+import TableHeader from './TableHeader.vue'
+import UsersTableHeader from './UsersTableHeader.vue'
+import HeaderRequestAdmin from './HeaderProjectsAdmin.vue'
+
+export default {
+  components: {
+    Table,
+    TableHeader,
+    HeaderRequestAdmin,
+    UsersTableHeader
+  },
+  data () {
+    return {
+      items: [
+        {name: 'projectName', label: 'Project Name'}, {name: 'designer', label: 'Designer'}
+      ],
+      columns: [
+        {prop: 'projectName', label: 'Project Name'}, {prop: 'designer', label: 'Designer'},
+        {prop: 'consultant', label: 'Consultant'}, {prop: 'projectSize', label: 'Project Size'},
+        {prop: 'equipment', label: 'Equipment'}, {prop: 'year', label: 'Year'}
+      ],
+      fields: [
+        {
+          projectName: 'Project 1',
+          designer: 'Tom Tomas',
+          consultant: 'Karl Marx',
+          projectSize: '12330',
+          equipment: '12330',
+          year: 2018
+        }, {
+          projectName: 'Project 2',
+          designer: 'Hail Tomas',
+          consultant: 'DEEN',
+          projectSize: '30',
+          equipment: '212330',
+          year: 2016
+        }, {
+          projectName: 'Project 1',
+          designer: 'Tom Tomas',
+          consultant: 'Karl Marx',
+          projectSize: '12330',
+          equipment: '12330',
+          year: 2018
+        }, {
+          projectName: 'Project 1',
+          designer: 'Tom Tomas',
+          consultant: 'Karl Marx',
+          projectSize: '12330',
+          equipment: '12330',
+          year: 2018
+        }, {
+          projectName: 'Project 1',
+          designer: 'Tom Tomas',
+          consultant: 'Karl Marx',
+          projectSize: '12330',
+          equipment: '12330',
+          year: 2018
+        }
+      ],
+      columnsUsers: [{prop: 'userName', label: 'User Name'}, {prop: 'email', label: 'Email'},
+        {prop: 'name', label: 'Name'}, {prop: 'groups', label: 'Groups'},
+        {prop: 'roles', label: 'Roles'}],
+      fieldsUsers: [{
+        userName: 'User Name',
+        email: 'John.Doesgmail.com',
+        name: 'Full Name',
+        groups: 'Group1',
+        roles: 'Sys. Admin',
+        status: 'Recent',
+        statusIcon: 'el-icon-time',
+        primary: true,
+        verified: true
+      }, {
+        userName: 'User Name',
+        email: 'John.Doesgmail.com',
+        name: 'Full Name',
+        groups: 'Group1',
+        roles: 'Sys. Admin',
+        status: 'Repeat',
+        statusIcon: 'el-icon-refresh',
+        primary: false,
+        verified: true
+      }, {
+        userName: 'User Name',
+        email: 'John.Doesgmail.com',
+        name: 'Full Name',
+        groups: 'Group1',
+        roles: 'Sys. Admin',
+        status: 'Inactive',
+        statusIcon: 'el-icon-remove-outline',
+        primary: false,
+        verified: true
+      }, {
+        userName: 'User Name',
+        email: 'John.Doesgmail.com',
+        name: 'Full Name',
+        groups: 'Group1',
+        roles: 'Sys. Admin',
+        status: 'Prospect',
+        statusIcon: 'el-icon-info',
+        primary: false,
+        verified: false
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '../../assets/font/fontello/css/fontello.css';
+  @import '../../assets/styles/varibles';
+
+  .header {
+    background-color: $blue-my-projects;
+    color: $white;
+
+    .my-projects-icon {
+      margin-top: auto;
+      margin-bottom: auto;
+      padding-left: 0.5em;
+      font-size: 1.5em;
+    }
+
+    .projects-label {
+      float: left;
+      font-weight: 500;
+      font-size: 1em;
+      padding-top: 0.5em;
+      padding-bottom: 0.5em;
+    }
+  }
+</style>
---
+++ src/components/partials/HeaderRequestAdmin.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="header-div">
+    <el-tabs type="border-card">
+      <el-tab-pane>
+        <div slot="label" class="header">
+          <div class="my-projects-icon"><i class="icon-project"></i></div>
+          <div class="projects-label">PRODUCTS AND MANUFACTURERS</div>
+          lala
+        </div>
+      </el-tab-pane>
+      <el-tab-pane class="tab">
+        <div slot="label" class="header">
+          <div class="my-projects-icon"><i class="el-icon-picture"></i></div>
+          <div class="projects-label">PRODUCT REQUEST</div>
+          lala
+        </div>
+      </el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
+<script>
+import UsersTableHeader from './UsersTableHeader.vue'
+export default {
+  components: {
+    UsersTableHeader
+  },
+  props: ['fields', 'columns', 'selectable', 'editable', 'fieldsUsers', 'columnsUsers', 'selectableUsers', 'editableUsers'],
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '../../assets/font/fontello/css/fontello.css';
+  @import '../../assets/styles/varibles';
+  .header {
+    display: flex;
+    flex-direction: row;
+    .my-projects-icon {
+      font-size: 1.5em;
+      padding-right: 1em;
+    }
+    .projects-label {
+      font-weight: 500;
+      font-size: 1em;
+    }
+  }
+</style>
