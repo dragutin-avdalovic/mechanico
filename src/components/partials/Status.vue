aad245032dfb22f2676f093bf35cb7c40827a9e8
Irma Rustemovic <irma.rustemovic@walter.ba>
2018-05-08 10:23:56 +0200

    status fix

--- src/components/partials/Status.vue
+++ src/components/partials/Status.vue
@@ -4,20 +4,22 @@
       <el-col>
         <div class="edit"><i class="el-icon-edit"></i></div>
       </el-col>
-      <el-col :span="12">
+    </el-row>
+    <el-row>
+      <el-col :span="10">
         <div class="center">
           <img class="profile-img" src="../../../static/images/img_avatar2.png" alt="Avatar">
         </div>
       </el-col>
-      <el-col :span="12" class="">
+      <el-col :span="14" class="">
         <div class="status">
           <span class=""><h3 style="margin: 0;">John Doe</h3></span>
           <span class="info">Company Name</span>
         </div>
         <div class="status contact-info">
-          <span class="info"><i class="icon-location"></i>London, UK</span>
-          <span class="info"><i class="icon-mail"></i>John.Doe@mail.com</span>
-          <span class="info"><i class="icon-phone"></i>+38700000000</span>
+          <span class="info icon"><i class="icon-location"></i>London, UK</span>
+          <span class="info icon"><i class="icon-mail"></i>John.Doe@mail.com</span>
+          <span class="info icon"><i class="icon-phone"></i>+38700000000</span>
         </div>
       </el-col>
     </el-row>
@@ -73,7 +75,7 @@ export default {
 
   .divider {
     border: 0.5px solid $grey-my-projects-darker;
-    margin: 1em 3em 1em 3em;
+    margin: 1em 2em 1em 2em;
   }
 
   .status {
@@ -91,6 +93,9 @@ export default {
     padding-bottom: 0.5em;
     font-size: 0.8em;
   }
+  .icon{
+    font-size: 0.9em;
+  }
 
   .circle {
     margin-bottom: 1em;
@@ -104,7 +109,7 @@ export default {
     font-size: 1.4em;
   }
   i{
-    margin-right: 1em;
+    margin-right: 0.5em;
   }
   .center{
     display: flex;
