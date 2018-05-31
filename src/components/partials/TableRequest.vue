02288d5bad20ce45515646b0210a9fe25aa7b96a
dragutin-avdalovic <dragutin.avdalovic@walter.ba>
2018-05-31 13:39:29 +0200

    Top of admin page height fixed, everything is in line, the padding fixed, tabs fixed.... whole page adjusted...  ---main user page fix of height

--- src/components/partials/TableRequest.vue
+++ src/components/partials/TableRequest.vue
@@ -8,7 +8,7 @@
     element-loading-background="rgba(0, 0, 0, 0.8)"
     :data="fields"
     style="width: 100%; overflow: auto;"
-    :height="'calc(45vh - 116px - 1em)'"
+    :height="'calc(45vh - 100px)'"
     @selection-change="handleSelectionChange">
     <el-table-column :width="width" v-if="selectable" type="selection"></el-table-column>
     <el-table-column
